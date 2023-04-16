a923d5346729d14a1c5489dc0e3f2b7b57355700
Zyree <123345456+Spirizeon@users.noreply.github.com>
2023-04-16 14:58:32 +0530

    variable names changed
    changed variable and function names for better readibility

--- harley_unopt.py
+++ harley_unopt.py
@@ -1,9 +1,3 @@
-'''
-█░█ ▄▀█ █▀█ █░░ █▀▀ █▄█   █▀▀ █▀█ █▀█   █░█░█ █ █▄░█ █▀▄ █▀█ █░█░█ █▀
-█▀█ █▀█ █▀▄ █▄▄ ██▄ ░█░   █▀░ █▄█ █▀▄   ▀▄▀▄▀ █ █░▀█ █▄▀ █▄█ ▀▄▀▄▀ ▄█
-𝒃𝒖𝒊𝒍𝒕 𝒃𝒚 𝑨𝒚𝒖𝒔𝒉; (𝒃𝒓𝒂𝒗𝒐𝒔𝒊𝒄𝒌𝒛@𝒈𝒊𝒕𝒉𝒖𝒃)
-'''
-
 
 import pyttsx3
 import speech_recognition as sr
@@ -11,124 +5,98 @@ import datetime
 import os
 import sys
 import time 
-from tkinter import Menu
-import psutil
-import pystray
-import PIL.Image
-import subprocess
 version = "a1.4"
 
-    #cpustats = psutil.cpu_percent()
-    #ramstats =  psutil.virtual_memory().percent
-            
-
-
-    
-        
-            
-            
-            
-        
-        
-            
-            
-            
-            
-        ))
-    
-#MUST BE RUN IN PROGRAM FILES INSIDE THE HARLEY.DA DIRECTORY
-engine = pyttsx3.init('sapi5') #microsoft api sapi5
+engine = pysttx3.init('sapi5') 
 voices = engine.getProperty('voices')
-print(voices[1].id) 
-engine.setProperty('voice', voices[1].id) #zira voice
-def nexrem(big,small):
-    from winotify import Notification, audio
-    ping = Notification(app_id="Harley: personal assistant",
-                        title=big,
-                        msg=small,
-                        icon = r"C:\Program Files\Harley.DA\harl.svg",
-                        duration="short")
+print(voices[1].id)
+def NOTIFICATION_SILENT(Heading, Description):
+	from winotify import notification, audio
+	NOTIFY = Notification(app id="Harley: personal assistant", title = Heading, msg = Description, icon = r"C:\Program Files\Harley.DA\harl.svg",duration = "short")
+	NOTIFY.show()
 
-    #ping.set_audio(audio.LoopingAlarm6,loop=False)
-    ping.show()
-def nexremlarm(big,small):
-    from winotify import Notification, audio
-    ping = Notification(app_id="Harley: personal assistant",
-                        title=big,
-                        msg=small,
-                        icon = r"C:\Program Files\Harley.DA\harl.svg",
-                        duration="long")
+def NOTIFICATION_ALARM(Heading, Description):
+	from winotify import notification, audio
+	NOTIFY = Notification(app id="Harley: personal assistant", title = Heading, msg = Description, icon = r"C:\Program Files\Harley.DA\harl.svg",duration = "short")
+	NOTIFY.show()
+	NOTIFY.set_audio(audio.LoopingAlarm6,loop=False)
+   	NOTIFY.add_actions(label="Dismiss")
+    	NOTIFY.show()
 
-    ping.set_audio(audio.LoopingAlarm6,loop=False)
-    ping.add_actions(label="Dismiss")
-    ping.show()
-def clearconsol():
-    import subprocess
-    import os
-    import sys
-    import time
-    time.sleep(2)
-    os.system("cls") #clear the console
-    print("*****************************")
-def speak(audio):
-    engine.say(audio)
-    engine.runAndWait()
-def preak(s):
-    nexrem("Harley",s)
-    speak(s)
-    print(s)
-def ear():
-    k = 0
-    while k < 1:
-        r = sr.Recognizer()
-        with sr.Microphone() as source: 
-            r.adjust_for_ambient_noise(source, duration=0.5)
-            r.pause_threshold = 1 #duration for how long it records
-            audio = r.listen(source) #listen
-        try:
-            print("Recognizing...")
-            query = r.recognize_google(audio, language='en-in') #translate stt
-            print(query) #result
-            return query
-            break
-        except Exception as e:
-            continue
-def glassear(): #ear() but without print functions, good for bg listening
-    k = 0
-    while k < 1:
-        r = sr.Recognizer()
-        with sr.Microphone() as source:
-            r.pause_threshold = 1 #duration for how long it records
-            audio = r.listen(source) #listen
-        try:
-            query = r.recognize_google(audio, language='en-in') #translate stt
-            return query
-            break
-        except Exception as e:
-            continue
-#NOTE: function to kill a running program through cmd input only works for .exe version
-def killself():
-    os.system("taskkill /f /im "+ "harley_unopt.exe" +" /t")
-def woofers():
-    import pywhatkit 
-    preak("Song?") 
-    comm = ear()
-    msg = "Sure, playing " + comm 
-    preak(msg)
-    pywhatkit.playonyt(comm)
-def greet():
-    n = datetime.datetime.now()
-    strmsg = "it is " + n.strftime("%Y-%m-%d %H:%M")
-    if n.hour in range(4,12):
-        preak("Good morning!")
-        nexrem('Harley',strmsg)
-    #now.strftime("%Y-%m-%d %H:%M:%S"))
-    elif n.minute in range(12,5):
-        preak("Good afteroon!")
-        preak(strmsg)
-    else:
-        preak("Good evening")
-        preak(strmsg)
+def CLEARSCREEN():
+	import os
+	import sys
+	import time
+	time.sleep(2)
+	os.system("cls") 
+	print("**************")
+
+def DICTATE(audio):
+	engine.say(audio)
+	engine.runAndWait()
+
+def DICTATE_CAPTIONS(captions):
+	NOTIFICATION_SILENT("Harley",captions) 
+	DICTATE(captions)
+	print(captions) 
+
+def SpeechToText():
+	while True:
+		Understanding = sr.Recogniser()
+		with sr.Microphone as source:
+			Understanding.adjust_for_ambient_noise(source, duration = 0.5)
+			Undestanding.pause_threshold = 1
+			audio = Understanding.listen(source)
+		try:
+			print("Recognizing...")
+			TRANSLATION = r.recognize_google(audio, language = 'en-in') 
+			print(TRANSLATION)
+			return TRANSLATION
+			break
+		except Exception as e:
+			continue
+			
+def promptless_SpeechToText():
+	while True:
+	Understanding = sr.Recogniser()
+	with sr.Microphone as source:
+		Understanding.adjust_for_ambient_noise(source, duration = 0.5)
+		Undestanding.pause_threshold = 1
+		audio = Understanding.listen(source)
+	try:
+		TRANSLATION = r.recognize_google(audio, language = 'en-in') 
+		return TRANSLATION
+		break
+	except Exception as e:
+		continue
+
+def kill_app():
+	os.system("taskill /f /im " + "harley_unopt.exe" + " /t")
+	
+def music_play():
+	import pywhatkit
+	DICTATE_CAPTIONS("Song?")
+	songname = SpeechToText()
+	msg = "Sure, playing " + songname
+	pywhatkit.playonyt(songname)
+
+def greet_user():
+	current_time = datetime.datetime.now()
+	time_prompt = "it is " + n.strftime("%Y-%m-%d %H:%M")
+	if current_time.hour in range(4,12):
+		DICTATE_CAPTIONS("Good morning!")
+		NOTIFICATION_SILENT('Harley', time_prompt)
+	elif current_time.minute in range(12,5):
+	        DICTATE_CAPTIONS("Good afteroon!")
+	        DICTATE_CAPTIONS(time_prompt)
+	 else:
+	        DICTATE_CAPTIONS("Good evening")
+	        DICTATE_CAPTIONS(time_prompt)
+	  
+
+			
+# -----------------> edit from here
+
 def gewgle():
     preak("what do you want to google?")
     y = str(ear())
